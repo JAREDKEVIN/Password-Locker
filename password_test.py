cbaba81c00543834998f53b1299e7e99d0d561b6
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-24 19:46:13 +0300

    test credentials

--- password_test.py
+++ password_test.py
@@ -1,3 +1,10 @@
 import unittest # Importing the unittest module
 from credentials import Credentials
 from user import User
+
+class TestCredentials(unittest.TestCase):
+    '''hfbhbhfbvhbfhvbhfvb
+    '''
+    def setUp(self):
+        """fhghgithgu"""
+        self.new_credentials =Credentials("pintres", "pepe" , "pipi")
