e94a0d3f46ef6ea672eb41399faf1639dbfe2fca
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-25 10:47:24 +0300

    save and add credentials

--- credentials.py
+++ credentials.py
@@ -11,4 +11,11 @@ def __init__ (self,account_name,account_username,account_password):
         self.account_name = account_name
         self.account_username = account_username
         self.account_password = account_password
-    
+        
+def save_credentials(self):
+
+        '''
+        test case to test if accounts objects is saved in accountlist
+        '''
+
+        Credentials.Accountlist.append(self)
