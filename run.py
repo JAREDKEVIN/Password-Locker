1799e6063842b4225f055cb827564b22ef93f62e
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-25 11:07:51 +0300

    create new user run

--- run.py
+++ run.py
@@ -4,3 +4,10 @@ from user import User
 import random
 import string
 
+# functions that implement the behaviours we have created in user
+def create_user(first_name,last_name,username,password):
+     '''
+    Function to create a new user
+    '''
+new_user = User(first_name,last_name,username,password)
+    return  new_user
