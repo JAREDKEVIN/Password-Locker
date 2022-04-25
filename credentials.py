0c4f5a54fc396f31469ad31577328dcb5decc132
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-25 10:49:39 +0300

    return user search by account name

--- credentials.py
+++ credentials.py
@@ -27,3 +27,17 @@ def delete_credentials(self):
         '''
 
         Credentials.Accountlist.remove(self)
+
+@classmethod
+def find_by_account_name(cls,account_name):
+            '''
+            Method that takes in a account_name and returns a account that matches that account_name.
+            Args:
+                account_name: user account_name to search for
+            Returns :
+                user of person that matches the account_name.
+            '''
+
+            for credential in cls.Accountlist:
+                if credential.account_name == account_name:
+                    return credential
