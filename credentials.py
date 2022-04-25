01127328b0d40697bc38779776ebb2a545b4e9f2
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-25 10:46:08 +0300

    function account details

--- credentials.py
+++ credentials.py
@@ -6,4 +6,9 @@ class Credentials:
     Class that generates new instances of credentials
     """
     Accountlist= [] #empty credential list
+
+def __init__ (self,account_name,account_username,account_password):
+        self.account_name = account_name
+        self.account_username = account_username
+        self.account_password = account_password
     
